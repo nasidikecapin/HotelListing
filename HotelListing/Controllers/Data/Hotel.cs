b1f6ae96b802d855bcd2d3db4cd5b97ad1a58bd3
nasidikecapin <nasidikecapin@gmail.com>
2021-10-26 08:33:27 +0800

    Initial check-in of module HotelListing

---
+++ HotelListing/Controllers/Data/Hotel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Controllers.Data
+{
+    public class Hotel
+    {
+        
+            public int id { get; set; }
+            public string Name { get; set; }
+            public string Address { get; set; }
+            public double Rating { get; set; }
+            [ForeignKey(nameof(Country))]
+            public Country Country { get; set; }
+            public int CountryId { get; set; }
+
+        
+    }
+}
